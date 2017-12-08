aa08b3833d628a9009349bb4df4f342b77eaf5e6
Sylvain Gogel <sylvain.gogel@gmail.com>
2017-12-08 18:44:55 +0100

    move to ES6 and babel to build process with browserify

--- test.js
+++ test.js
@@ -1,5 +1,7 @@
-var unique = require('uniq');
+import unique from 'uniq'
+import dotenv from 'dotenv'
+dotenv.config()
 
-var data = [1, 2, 2, 3, 4, 5, 5, 5, 6];
+var data = [1, 2, 2, 3, 4, 5, 5, 5, 6, process.env.APP_TEST]
 
-console.log(unique(data));
+console.log(unique(data))
